f3233cb0b7ae9e2a5386335e39ddc1e726829e1c
Emi <emilie.mathian@insa-lyon.fr>
2018-03-30 21:44:00 +0200

    Setters

--- Node.cpp
+++ Node.cpp
@@ -3,8 +3,18 @@ Node* Node::next(){
 	return next_;
 } 
 
- 
 
 MesComplexes* Node::obj(){
 	return obj_;
+}
+
+void MesComplexes::set_obj(MesComplexes* obj)
+{
+	obj_ = obj;
+}
+
+
+void MesComplexes::set_next(MesComplexes* next)
+{
+	next_ = next;
 }
