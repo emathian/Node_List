f6fb979e994d0d456002a079015a784a8cdc6e31
Emi <emilie.mathian@insa-lyon.fr>
2018-03-30 21:39:05 +0200

    Some comments

--- Node.h
+++ Node.h
@@ -1,3 +1,22 @@
 #include "MesComplexes.h"
 class Node {
+		/*
+	protected  :
+	Node * next_ ; 
+	MesComplexes * obj_ ;
+	
+
+	public:
+		
+		// Getteers
+		Node* next();
+		MesComplexes* obj();
+
+		
+		// Seters
+		MesComplexes* set_obj(MesComplexes* obj);
+		// nous voulons pouvoir modifier l'objet contenu dans la liste 
+		MesComplexes* set_next(MesComplexes* next);
+		
+	*/
 };
