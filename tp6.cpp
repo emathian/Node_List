db79e2c6364bea4d36590e942a146bde479c9e9f
Emi <emilie.mathian@insa-lyon.fr>
2018-03-30 22:12:34 +0200

    Test of setter

--- tp6.cpp
+++ tp6.cpp
@@ -6,11 +6,15 @@
 int main(){
 	
     MesComplexes C1(0 , -2);
+    MesComplexes C2;
     Node node1;
     std::cout << node1.next()<< std::endl;
     std::cout << node1.obj()<< std::endl;
     Node node2(&C1);
     std::cout << node2.next()<< std::endl;
     std::cout << node2.obj()<< std::endl;
+    node2.set_obj(&C2);
+    std::cout << node2.next()<< std::endl;
+    std::cout << node2.obj()<<"	C2 Address	"<< &C2 <<std::endl;
 	return 0;
 }
