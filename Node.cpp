ae8396570e28d29c6f3e3a7ee90840c6e1974ea7
Emi <emilie.mathian@insa-lyon.fr>
2018-03-31 15:26:52 +0200

    definition of Prev attribute

--- Node.cpp
+++ Node.cpp
@@ -7,11 +7,13 @@
 Node::Node()
 {
     next_ = nullptr;
+    prev_ = nullptr;
     obj_ = 0;
 }
 //Parameterized constructor
 Node::Node(MesComplexes* complexe)
 {
+	prev_ = nullptr;
     next_ = nullptr;
     obj_ = complexe;
 }
