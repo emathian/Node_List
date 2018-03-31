2337eecbc7cf52eb0d292657eeb1fed5e6b53411
Emi <emilie.mathian@insa-lyon.fr>
2018-03-31 13:42:17 +0200

    Version 1 Puschback

--- List.cpp
+++ List.cpp
@@ -15,6 +15,7 @@ int List::nb_elts()
 	return nb_elts_;
 }	
 
+
 //Constructor 
 //Default constructor
 List::List()
@@ -31,3 +32,32 @@ List::List(int number_elements)
 	head_ = nullptr;
 	nb_elts_ = number_elements;
 }
+
+//Methods
+void List::Pushback(MesComplexes add_complex)
+{
+	Node * temp = new Node(add_complex); //Create a tempory node
+	
+	Node *current = head_;
+	/*
+
+	if (current == nullptr)
+	{
+		head = temp;
+		nb_elts = 1;
+	}
+	else
+	{
+	*/
+	//l'attribut next du noeud != null ptr
+	while((*current).next_ != nullptr)
+	{
+		current = (*current).next_;
+		(*current).next_ = temp; 
+		//nb_elts ++;	
+	}
+
+}
+	//IF  il n'y a pas de noeud 
+
+	
